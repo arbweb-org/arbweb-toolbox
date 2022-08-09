948aee8a9c3a22d6eb156bbb92fbef41b9d35818
يس سليمان <yassin_soliman@outlook.com>
2022-08-09 16:27:03 +0200

    Project clean up

--- arbweb_toolbox/arbweb_toolbox_mobile/MauiProgram.cs
+++ arbweb_toolbox/arbweb_toolbox_mobile/MauiProgram.cs
@@ -1,27 +1,22 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using arbweb_toolbox_mobile.Data;
-
-namespace arbweb_toolbox_mobile;
+﻿namespace arbweb_toolbox_mobile;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		builder.Services.AddMauiBlazorWebView();
-		#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+
 #endif
-		
-		builder.Services.AddSingleton<WeatherForecastService>();
-
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
