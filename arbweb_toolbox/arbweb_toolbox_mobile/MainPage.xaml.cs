efbf13949b284e3ae60e2b2a55031c2596a5e40f
يس سليمان <yassin_soliman@outlook.com>
2022-08-09 14:16:01 +0200

    Created solution and mobile project

---
+++ arbweb_toolbox/arbweb_toolbox_mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace arbweb_toolbox_mobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ arbweb_toolbox/arbweb_toolbox_mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace arbweb_toolbox_mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
