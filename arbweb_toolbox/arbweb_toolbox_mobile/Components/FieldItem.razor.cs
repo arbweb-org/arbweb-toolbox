cf7569bb720bb355cef10f48791bbadefc85b8f7
يس سليمان <yassin_soliman@outlook.com>
2023-10-29 20:34:03 +0100

    Updated Notes page

--- arbweb_toolbox/arbweb_toolbox_mobile/Components/FieldItem.razor.cs
+++ arbweb_toolbox/arbweb_toolbox_mobile/Components/FieldItem.razor.cs
@@ -1,4 +1,30 @@
-﻿namespace arbweb_toolbox_mobile.Components
+﻿using Microsoft.AspNetCore.Components;
+using System.Globalization;
+
+namespace arbweb_toolbox_mobile.Components
 {
-    public partial class FieldItem : _c_component { }
+    public partial class FieldItem : _c_component
+    {
+        [Parameter]
+        public DateTime g_dat { get; set; }
+
+        [Parameter]
+        public string g_ttl { get; set; }
+
+        [Parameter]
+        public string g_icn { get; set; }
+
+        string f_date()
+        {
+            var l_clt = new CultureInfo("ar-ar");
+            var l_dat = g_dat.ToString("dddd d MMMM yyyy | h:m t", l_clt);
+
+            return l_dat;
+        }
+
+        string f_icon()
+        {
+            return $"img/Notes/{g_icn}.svg";
+        }
+    }
 }
