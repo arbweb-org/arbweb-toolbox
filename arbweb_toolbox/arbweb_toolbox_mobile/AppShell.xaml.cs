a574c4386e75aeba2736e9b1856301777f3694ff
يس سليمان <yassin_soliman@outlook.com>
2024-07-16 04:18:17 +0300

    Updated deployment info

---
+++ arbweb_toolbox/arbweb_toolbox_mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace arbweb_toolbox_mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ arbweb_toolbox/arbweb_toolbox_mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace arbweb_toolbox_mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
