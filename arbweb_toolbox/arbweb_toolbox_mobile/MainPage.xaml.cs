9f170e5c88b0091d387404c36bb2e1bfa74b740f
يس سليمان <yassin_soliman@outlook.com>
2024-07-17 03:11:47 +0300

    Updated home page

--- arbweb_toolbox/arbweb_toolbox_mobile/MainPage.xaml.cs
+++ arbweb_toolbox/arbweb_toolbox_mobile/MainPage.xaml.cs
@@ -1,5 +1,32 @@
-﻿namespace arbweb_toolbox_mobile
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace arbweb_toolbox_mobile
 {
+    public class _c_item
+    {
+        public string g_nam { get; set; }
+
+        public _c_item(string p_nam)
+        {
+            g_nam = p_nam;
+        }
+    }
+
+    public class _c_model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<_c_item> g_itm { get; private set; } =
+            new ObservableCollection<_c_item>
+            {
+                new _c_item("A"),
+                new _c_item("B"),
+                new _c_item("C"),
+                new _c_item("D")
+            };
+    }
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
